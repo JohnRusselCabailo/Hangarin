b56fa87c0d2573eb34c891b9bb27ca4c167e6b71
JohnRusselCabailo <202380167@psu.palawan.edu.ph>
2025-10-15 22:37:36 +0800

    added edit button

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app1.views import HomePageView, TaskList, TaskCreateView
+from app1.views import HomePageView, TaskList, TaskCreateView, TaskUpdateView
 from app1 import views
 
 urlpatterns = [
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('task_list', TaskList.as_view(), name='task-list'),
     path('task_list/add', TaskCreateView.as_view(), name='task-add'),
+    path('task_list/<pk>',TaskUpdateView.as_view(), name='task- update'),
 ]
